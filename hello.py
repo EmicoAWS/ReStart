736a3a9aa8264d39c52cfcb296a74538ed3ffcc8
EmicoAWS <117093372+EmicoAWS@users.noreply.github.com>
2022-11-02 11:45:58 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('Hello World!')
+print("Python has three numeric types: int, float, and complex")
+
+myValue='uraaaa'
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
